895ac2a44207739c623f3506b0c658b584c31541
wnghks7787 <wnghks7787@naver.com>
2026-06-02 16:20:27 +0900

    update .gitignore: add bin file

---
+++ src/client/client.c
@@ -0,0 +1,149 @@
+// CLIENT
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+
+#define MAXDATASIZE 10
+
+char* toString(int a)
+{
+	char* ret = malloc(sizeof(char) * 3);
+
+	ret[2] = '\0';
+	ret[1] = ((a%10) + '0');
+	a /= 10;
+	ret[0] = (a + '0');
+	
+	return ret;
+}
+
+int selectLED()
+{
+	int mode_val;
+	printf("\n---------- LED Control Mode ----------\n");
+	printf("Select your mode: \n");
+	printf("1. LED 100%% ON\n");
+	printf("2. LED 70%% ON\n");
+	printf("3. LED 50%% ON\n");
+	printf("4. LED 25%% ON\n");
+	printf("5. LED OFF\n");
+	
+	printf("What do you want to see: ");
+	scanf("%d", &mode_val);
+
+	return mode_val;
+}
+
+int selectCDS()
+{
+	int mode_val;
+	printf("\n---------- CDS Control Mode ----------\n");
+	printf("Select your mode: \n");
+	printf("1. Check CDS value\n");
+	printf("2. Control LED with CDS\n");
+
+	printf("What do you want to see: ");
+	scanf("%d", &mode_val);
+
+	return mode_val;
+}
+
+int selectMode()
+{
+	int mode_val;
+	printf("\n========== Device Management System ==========\n");
+	printf("Select your mode: \n");
+	printf("1. LED ON_OFF\n"); // 1X
+	printf("2. Buzzer\n"); // 20
+	printf("3. CDS\n"); // 3X
+	printf("4. 7-Segment\n"); // 40
+
+	printf("What do you want to see: ");
+	scanf("%d", &mode_val);
+
+	mode_val *= 10;
+
+	if(mode_val == 10)
+	{
+		mode_val += selectLED();
+	}
+	else if(mode_val == 30)
+	{
+		mode_val += selectCDS();
+	}
+
+	return mode_val;
+}
+
+int main(int argc, char* argv[])
+{
+	int sockfd, numbytes;
+	socklen_t addr_len;
+	char buf[MAXDATASIZE];
+	struct hostent *he;
+	struct sockaddr_in server_addr;
+
+	int mode_value;
+
+	// signal control
+	sigset_t set;
+
+	sigfillset(&set);
+	sigdelset(&set, SIGINT);
+	sigprocmask(SIG_SETMASK, &set, NULL);
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "usage: client hostname\n");
+		exit(1);
+	}
+	if((he = gethostbyname(argv[1])) == NULL)
+	{
+		perror("gethostbyname");
+		exit(1);
+	}
+	
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(60000);
+	server_addr.sin_addr = *((struct in_addr*)he->h_addr);
+	printf("[%s]\n", (char*)inet_ntoa(server_addr.sin_addr));
+	memset(&(server_addr.sin_zero), '\0', 8);
+
+	if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("connect");
+		exit(1);
+	}
+
+	while(1)
+	{
+		mode_value = selectMode();
+		strncpy(buf, toString(mode_value), 3);
+		printf("buf: %s\n", buf);
+
+		if(send(sockfd, buf, 3, 0) == -1)
+		{
+			perror("send");
+			close(sockfd);
+			exit(1);
+		}
+
+	}
+	close(sockfd);
+
+	return 0;
+}
---
+++ src/server/server.c
@@ -0,0 +1,153 @@
+// SERVER
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+
+//#include "devcontrol.h"
+
+#define BACKLOG 10
+#define MAXDATASIZE 100
+/*
+void* led_thread(void* arg)
+{
+	led_on((char*)arg);
+}
+*/
+void makedaemon()
+{
+	struct sigaction sa;
+	struct rlimit rl;
+	int fd0, fd1, fd2;
+	pid_t pid;
+
+	umask(0);
+
+	if(getrlimit(RLIMIT_NOFILE, &rl) < 0)
+	{
+		perror("getlimit");
+		exit(1);
+	}
+
+	if((pid = fork()) < 0)
+	{
+		perror("fork");
+		exit(1);
+	}
+	// close parent process
+	else if(pid != 0)
+	{
+		_exit(EXIT_SUCCESS);
+	}
+
+	setsid();
+
+	// signal handler
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	if(sigaction(SIGHUP, &sa, NULL) < 0)
+	{
+		perror("sigaction(): Can't ignore SIGHUP");
+		exit(1);
+	}
+
+	if(chdir("/"))
+	{
+		perror("cd");
+		exit(1);
+	}
+
+	// close all file descriptor
+	if(rl.rlim_max == RLIM_INFINITY)
+	{
+		rl.rlim_max = 1024;
+	}
+
+	for(int i = 0 ; i < rl.rlim_max ; i++)
+	{
+		close(i);
+	}
+
+	// make fd 0, 1, 2 to /dev/null
+	fd0 = open("/dev/null", O_RDWR);
+	fd1 = dup(0);
+	fd2 = dup(0);
+}
+
+int main()
+{
+	int sockfd, new_fd;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	int sin_size;
+	char buf[MAXDATASIZE];
+	int numbytes;
+	
+	pthread_t led_tid, buzzer_tid, cds_tid, seg_tid;
+
+	// make process daemon
+	makedaemon();
+
+	// socket
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(1);
+	}
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(60000);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	memset(&(server_addr.sin_zero), '\0', 8);
+	
+	// bind
+	if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("bind");
+		exit(1);
+	}
+
+	// listen
+	if(listen(sockfd, BACKLOG) == -1)
+	{
+		perror("listen");
+		exit(1);
+	}
+
+	// accept
+	sin_size = sizeof(struct sockaddr_in);
+	if((new_fd = accept(sockfd, (struct sockaddr*)&client_addr, &sin_size)) == -1)
+	{
+		perror("accept");
+		exit(1);
+	}
+	printf("server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
+
+	while(1)
+	{
+		if((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1)
+		{
+			perror("recv");
+			exit(1);
+		}
+		buf[numbytes] = '\0';
+
+		if(!(strcmp(buf, "10")))
+		{
+	//		pthread_create(&led_tid, NULL, led_thread, buf);
+			printf("test");
+		}
+		//pthread_join(led_tid, (void**)NULL);
+	}
+	return 0;
+}

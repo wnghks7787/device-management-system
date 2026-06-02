35c1bc92fa5285da904d934a7fd1dddf6e03e333
wnghks7787 <wnghks7787@naver.com>
2026-06-02 09:39:08 +0900

    add src files: server.c, client.c

---
+++ src/server.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+#define BACKLOG 10
+
+int main()
+{
+	int sockfd, new_fd;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	int sin_size;
+
+	// socket
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(1);
+	}
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(60000);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	memset(&(server_addr.sin_zero), '\0', 8);
+	
+	// bind
+	if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("bind");
+		exit(1);
+	}
+
+	// listen
+	if(listen(sockfd, BACKLOG) == -1)
+	{
+		perror("listen");
+		exit(1);
+	}
+
+	while(1)
+	{
+		sin_size = sizeof(struct sockaddr_in);
+		if((new_fd = accept(sockfd, (struct sockaddr*)&client_addr, &sin_size)) == -1)
+		{
+			perror("accept");
+			continue;
+		}
+		printf("server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
+		if(!fork())
+		{
+			close(sockfd);
+			if(send(new_fd, "Hello, client!\n", 14, 0) == -1)
+			{
+				perror("send");
+			}
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);
+		while(waitpid(-1, NULL, WNOHANG) > 0);
+	}
+	return 0;
+}

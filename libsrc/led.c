c48b2231aad4ff5b7c98709619c04cd1d6d33cad
wnghks7787 <wnghks7787@naver.com>
2026-06-04 19:49:02 +0900

    make code easy

--- libsrc/led.c
+++ libsrc/led.c
@@ -7,28 +7,32 @@
 
 #define LED 29
 
+#define LED_100 255
+#define LED_75 191
+#define LED_50 127
+#define LED_25 63
+#define LED_0 0
+
 void led_control(char* mode)
 {
-	softPwmCreate(LED, 0, 255);
-
 	if(strcmp(mode, "11") == 0)
 	{
-		softPwmWrite(LED, 255);
+		softPwmWrite(LED, LED_100);
 	}
 	else if(strcmp(mode, "12") == 0)
 	{
-		softPwmWrite(LED, 191);
+		softPwmWrite(LED, LED_75);
 	}
 	else if(strcmp(mode, "13") == 0)
 	{
-		softPwmWrite(LED, 127);
+		softPwmWrite(LED, LED_50);
 	}
 	else if(strcmp(mode, "14") == 0)
 	{
-		softPwmWrite(LED, 63);
+		softPwmWrite(LED, LED_25);
 	}
 	else
 	{
-		softPwmWrite(LED, 0);
+		softPwmWrite(LED, LED_0);
 	}
 }
